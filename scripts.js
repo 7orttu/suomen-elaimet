96b242d3c24568bd5e6c91bc6ec7945febc165a9
7orttu <hillmanniklas@gmail.com>
2024-03-18 14:55:48 +0200

    Image preview scenes added for each animal

--- scripts.js
+++ scripts.js
@@ -9,25 +9,50 @@ const scenes = [
         image: "Materials/karhu-pääkuva.jpg",
         text: "ohjeet"
     },
+    {   // preview
+        name:"Susi-pre",
+        image: "Materials/susi-sisältökuva.jpg",
+        text: ""
+    },
     {
         name: "Susi",
         image: "Materials/susi-sisältökuva.jpg",
         text: "Sudesta fakta"
+    },
+    {   // preview
+        name: "Kettu-pre",
+        image: "Materials/kettu-sisältökuva.jpg",
+        text: ""
     },
     {
         name: "Kettu",
         image: "Materials/kettu-sisältökuva.jpg",
         text: "Ketusta fakta"
     },
+    {   // preview
+        name: "Karhu-pre",
+        image: "Materials/karhu-sisältökuva.jpg",
+        text: ""
+    },
     {
         name: "Karhu",
         image: "Materials/karhu-sisältökuva.jpg",
         text: "Karhusta fakta"
     },
+    {   // preview
+        name: "Metsäkauris-pre",
+        image: "Materials/metsäkauris-sisältökuva.jpg",
+        text: ""
+    },
     {
         name: "Metsäkauris",
         image: "Materials/metsäkauris-sisältökuva.jpg",
         text: "Metsäkauriksesta fakta"
+    },
+    {   // preview
+        name: "Hirvi-pre",
+        image: "Materials/hirvi-sisältökuva.jpg",
+        text: ""
     },
     {
         name: "Hirvi",
