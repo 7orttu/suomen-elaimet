05cf5f13862ae6921eade947c461e5c9df3d2b72
7orttu <hillmanniklas@gmail.com>
2024-04-08 13:25:35 +0300

    Added credits for images into scenes array

--- data.js
+++ data.js
@@ -3,91 +3,106 @@ const scenes = [
         name: "main",
         image: "Materials/resized-pictures/main.jpg",
         text: "Suomen Eläimet",
-        buttonText: ["", "ALOITA"]
+        buttonText: ["", "ALOITA"],
+        creditText: "LTapsaH, Pixabay"
     },
     {
         name: "ohjeet",
         image: "Materials/resized-pictures/main.jpg",
         text: "OHJEET \n Paina 'seuraava' ja 'edellinen' nappeja vaihtaaksesi ruutuja!",
-        buttonText: ["TAKAISIN", "JATKA"]
+        buttonText: ["TAKAISIN", "JATKA"],
+        creditText: "LTapsaH, Pixabay"
     },
     {   // preview
         name:"Susi-pre",
         image: "Materials/resized-pictures/wolf.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "christels, Pixabay"
     },
     {
         name: "Susi",
         image: "Materials/resized-pictures/wolf.jpg",
         text: "SUSI \n Tiesitkö, että susi on yöeläin? Tämä tarkoittaa että ne liikkuvat pääosin öisin ja hämärällä. Sudet myös saalistavat usein yöllä.",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "christels, Pixabay"
     },
     {   // preview
         name: "Kettu-pre",
         image: "Materials/resized-pictures/fox.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "azazelok, Pixabay"
     },
     {
         name: "Kettu",
         image: "Materials/resized-pictures/fox.jpg",
         text: "KETTU \n Noin 10% Suomen ketuista on 'hopeakettuja', niiden turkin väri vaihtelee hopeanvärisestä lähes mustaan.",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "azazelok, Pixabay"
     },
     {   // preview
         name: "Karhu-pre",
         image: "Materials/resized-pictures/bear.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "Bergadder, Pixabay"
     },
     {
         name: "Karhu",
         image: "Materials/resized-pictures/bear.jpg",
         text: "KARHU \n Tiesitkö, että karhu ei ole ainoastaan Suomen suurin petoeläin, vaan koko Euroopan!",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "Bergadder, Pixabay"
     },
     {   // preview
         name: "Metsäkauris-pre",
         image: "Materials/resized-pictures/roedeer.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "Luboshouska, Pixabay"
     },
     {
         name: "Metsäkauris",
         image: "Materials/resized-pictures/roedeer.jpg",
         text: "METSÄKAURIS \n Metsäkauriit ovat taitavia uimareita!",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "Luboshouska, Pixabay"
     },
     {   // preview
         name: "Hirvi-pre",
         image: "Materials/resized-pictures/moose.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "12019, Pixabay"
     },
     {
         name: "Hirvi",
         image: "Materials/resized-pictures/moose.jpg",
         text: "HIRVI \n Aikuinen hirvi voi painaa jopa huimat 800 kilogrammaa! Se on noin kolme moottoripyörää!",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "12019, Pixabay"
     },
     {   // preview
         name: "Majava-pre",
         image: "Materials/resized-pictures/beaver.jpg",
         text: "",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "SteveRaubenstine, Pixabay"
     },
     {
         name: "Majava",
         image: "Materials/resized-pictures/beaver.jpg",
         text: "MAJAVA \n Tiesitkö, että majavat voivat elää jopa yli 20 vuotiaiksi?",
-        buttonText: ["TAKAISIN", "SEURAAVA"]
+        buttonText: ["TAKAISIN", "SEURAAVA"],
+        creditText: "SteveRaubenstine, Pixabay"
     },
     {
         name: "end",
         image: "Materials/resized-pictures/main.jpg",
         text: "LOPPU",
-        buttonText: ["TAKAISIN", "LOPETA"]
+        buttonText: ["TAKAISIN", "LOPETA"],
+        creditText: "LTapsaH, Pixabay"
     }
 ];
 
