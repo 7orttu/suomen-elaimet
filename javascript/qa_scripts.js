c0380e58df554c3989c92ce9f14480041b231e00
7orttu <hillmanniklas@gmail.com>
2024-04-10 14:17:48 +0300

    Question scripts top level declarations

---
+++ javascript/qa_scripts.js
@@ -0,0 +1,16 @@
+import { qaQuestions } from '../javascript/data.js';
+// import { scenes } from '../javascript/data.js';
+
+
+
+let questionContainer = document.getElementById('question-container');
+let questionText = document.getElementById('question');
+let imageContainer = document.getElementById('image-container');
+let animalImage = document.getElementById('animal-image');
+let imageCredit = document.getElementById('image-credit');
+
+
+
+let currentQuestionIndex = 0;
+
+
