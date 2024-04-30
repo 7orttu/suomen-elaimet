2220ea51dac7c18fa8dc28a8cabbba0c5ad666aa
7orttu <hillmanniklas@gmail.com>
2024-04-30 14:16:40 +0300

    Continuing rework
    "qa_scripts.js" element variables imported to "scripts.js".
    Setting visibilities and enabling elements.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,7 @@
 import { scenes } from '../javascript/data.js';
 import { qaQuestions } from '../javascript/data.js';
 
+// FACT PART
 let prevBtn = document.getElementById('prev-btn');
 let nextBtn = document.getElementById('next-btn');
 let restartBtn = document.getElementById('restart-btn');
@@ -14,8 +15,24 @@ let imageCredit = document.getElementById('image-credit');
 
 let textContainer = document.getElementById('text-container');
 let imageContainer = document.getElementById('image-container');
+let controlContainer = document.getElementById('control-container');
 let languageContainer = document.getElementById('language-container');
 let fPartEnd = document.getElementById('fpartend-container');
+
+
+// QUESTION PART
+let optionBtnF = document.getElementById('option1');
+let optionBtnS = document.getElementById('option2');
+let optionBtnT = document.getElementById('option3');
+let optionBtnFO = document.getElementById('option4');
+
+let optionContainer = document.getElementById('option-container');
+
+let endTxt = document.getElementById('end-txt');
+let endBtn = document.getElementById('end-btn');
+
+const optionButtons = document.querySelectorAll('.optionButtons');
+
 
 
 let currentSceneIndex = 0;
@@ -37,6 +54,8 @@ function DisplayScene(sceneIndex) {
 
         ElementEnabler(prevBtn, true, "hidden");
         ElementEnabler(nextBtn, false, "visible");
+
+        ElementEnabler(optionContainer, true, "hidden")
 
         ElementLocationModifier(nextBtn, "", "-10px", "", "13%");
         ElementLocationModifier(prevBtn, "", "-80px", "", "-22%");
@@ -61,6 +80,8 @@ function DisplayScene(sceneIndex) {
         ElementEnabler(prevBtn, false, "visible");
         ElementEnabler(nextBtn, false, "visible");
 
+        ElementEnabler(optionContainer, true, "hidden")
+
         ElementLocationModifier(textContainer, "75px", "", "15px");
         ElementSizeModifier(textContainer, "250px");
         textContainer.style.backgroundColor = 'white';
@@ -80,6 +101,8 @@ function DisplayScene(sceneIndex) {
 
         ElementEnabler(prevBtn, false, "visible");
         ElementEnabler(nextBtn, false, "visible");
+
+        ElementEnabler(optionContainer, true, "hidden")
 
         ElementLocationModifier(textContainer, "50px", "", "0px");
         ElementSizeModifier(textContainer, "700px");
@@ -101,6 +124,8 @@ function DisplayScene(sceneIndex) {
         ElementEnabler(prevBtn, false, "visible");
         ElementEnabler(nextBtn, false, "visible");
 
+        ElementEnabler(optionContainer, true, "hidden")
+
         ElementLocationModifier(textContainer, "220px");
         ElementSizeModifier(textContainer, "700px");
         textContainer.style.backgroundColor = 'white';
@@ -120,6 +145,8 @@ function DisplayScene(sceneIndex) {
 
         ElementEnabler(prevBtn, false, "visible");
         ElementEnabler(nextBtn, true, "hidden");
+
+        ElementEnabler(optionContainer, true, "hidden")
 
         ElementLocationModifier(textContainer, "50px");
         ElementSizeModifier(textContainer, "700px");
@@ -150,11 +177,22 @@ function DisplayQuestion(questionIndex) {
     const currentQuestion = qaQuestions[questionIndex];
 
     animalImage.src = currentQuestion.image;
-    // scene.style.backgroundImage = "url('" + currentScene.bgImage + "')";
     animalText.innerText = currentQuestion.question;
     // prevBtn.innerText = currentScene.buttonText[0];
     // nextBtn.innerText = currentScene.buttonText[1];
     imageCredit.innerText = currentQuestion.creditText;
+
+    if(currentQuestion.name === "wolfQuestion" || currentQuestion.name === "foxQuestion" || currentQuestion.name === "bearQuestion" || currentQuestion.name === "roedeerQuestion" || currentQuestion.name === "mooseQuestion" || currentQuestion.name === "beaverQuestion") {
+        ElementEnabler(optionContainer, false, "visible");
+
+        ElementEnabler(fPartEnd, true, "hidden");
+        
+        ElementEnabler(endBtn, true, "hidden");
+        ElementEnabler(endTxt, true, "hidden");
+    }
+    else {
+        
+    }
 }
 
 function ElementEnabler(element, isdisabled, visibility) {
@@ -190,7 +228,7 @@ document.getElementById('restart-btn').addEventListener('click', function() {   
 });
 
 document.getElementById('continue-btn').addEventListener('click', function() {  // CONTINUE
-    currentQuestionIndex = (currentQuestionIndex + 1) % qaQuestions.length;
+    // currentQuestionIndex = (currentQuestionIndex + 1) % qaQuestions.length;
     DisplayQuestion(currentQuestionIndex);
 });
 
